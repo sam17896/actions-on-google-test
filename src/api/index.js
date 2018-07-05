d9e9b0d3e91f4a135cae828546eb03a2bdae8d40
Sohail <asohail4@trgworld.com>
2018-07-05 22:34:44 +0500

    test9

--- src/api/index.js
+++ src/api/index.js
@@ -27,39 +27,54 @@ export default ({ config, db }) => {
 		// 	permissions: ['NAME', 'DEVICE_PRECISE_LOCATION'],
 		// };
 		// conv.ask(new Permission(options));
+
+		console.log(req);
         var result = {
-			"fulfillmentText": "This is a text response",
-			"fulfillmentMessages": [
-			  {
-				"card": {
-				  "title": "card title",
-				  "subtitle": "card text",
-				  "imageUri": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
-				  "buttons": [
-					{
-					  "text": "button text",
-					  "postback": "https://assistant.google.com/"
-					}
-				  ]
+		"speech": "this text is spoken out loud if the platform supports voice interactions",
+		"displayText": "this text is displayed visually",
+		"messages": {
+		  "type": 1,
+		  "title": "card title",
+		  "subtitle": "card text",
+		  "imageUrl": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png"
+		},
+		"data": {
+		  "google": {
+			"expectUserResponse": true,
+			"richResponse": {
+			  "items": [
+				{
+				  "simpleResponse": {
+					"textToSpeech": "this is a simple response"
+				  }
 				}
-			  }
-			],
-			"source": "example.com",
-			"payload": {
-			  "google": {
-				"expectUserResponse": true,
-				"richResponse": {
-				  "items": [
-					{
-					  "simpleResponse": {
-						"textToSpeech": "this is a simple response"
-					  }
-					}
-				  ]
-				}
-			  },
-			},
-		  };
+			  ]
+			}
+		  },
+		  "facebook": {
+			"text": "Hello, Facebook!"
+		  },
+		  "slack": {
+			"text": "This is a text response for Slack."
+		  }
+		},
+		"contextOut": [
+		  {
+			"name": "context name",
+			"lifespan": 5,
+			"parameters": {
+			  "param": "param value"
+			}
+		  }
+		],
+		"source": "example.com",
+		"followupEvent": {
+		  "name": "event name",
+		  "parameters": {
+			"param": "param value"
+		  }
+		}
+	  };
 
 		console.log(result);
 		res.send(result);
