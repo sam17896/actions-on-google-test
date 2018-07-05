37e6e554e5cf33aed61fe1873a97061d60851f6c
Sohail <asohail4@trgworld.com>
2018-07-05 22:41:21 +0500

    test10

--- src/api/index.js
+++ src/api/index.js
@@ -30,51 +30,60 @@ export default ({ config, db }) => {
 
 		console.log(req);
         var result = {
-		"speech": "this text is spoken out loud if the platform supports voice interactions",
-		"displayText": "this text is displayed visually",
-		"messages": {
-		  "type": 1,
-		  "title": "card title",
-		  "subtitle": "card text",
-		  "imageUrl": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png"
-		},
-		"data": {
-		  "google": {
-			"expectUserResponse": true,
-			"richResponse": {
-			  "items": [
-				{
-				  "simpleResponse": {
-					"textToSpeech": "this is a simple response"
-				  }
+			"fulfillmentText": "This is a text response",
+			"fulfillmentMessages": [
+			  {
+				"card": {
+				  "title": "card title",
+				  "subtitle": "card text",
+				  "imageUri": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
+				  "buttons": [
+					{
+					  "text": "button text",
+					  "postback": "https://assistant.google.com/"
+					}
+				  ]
 				}
-			  ]
+			  }
+			],
+			"source": "example.com",
+			"payload": {
+			  "google": {
+				"expectUserResponse": true,
+				"richResponse": {
+				  "items": [
+					{
+					  "simpleResponse": {
+						"textToSpeech": "this is a simple response"
+					  }
+					}
+				  ]
+				}
+			  },
+			  "facebook": {
+				"text": "Hello, Facebook!"
+			  },
+			  "slack": {
+				"text": "This is a text response for Slack."
+			  }
+			},
+			"outputContexts": [
+			  {
+				"name": "projects/${PROJECT_ID}/agent/sessions/${SESSION_ID}/contexts/context name",
+				"lifespanCount": 5,
+				"parameters": {
+				  "param": "param value"
+				}
+			  }
+			],
+			"followupEventInput": {
+			  "name": "event name",
+			  "languageCode": "en-US",
+			  "parameters": {
+				"param": "param value"
+			  }
 			}
-		  },
-		  "facebook": {
-			"text": "Hello, Facebook!"
-		  },
-		  "slack": {
-			"text": "This is a text response for Slack."
 		  }
-		},
-		"contextOut": [
-		  {
-			"name": "context name",
-			"lifespan": 5,
-			"parameters": {
-			  "param": "param value"
-			}
-		  }
-		],
-		"source": "example.com",
-		"followupEvent": {
-		  "name": "event name",
-		  "parameters": {
-			"param": "param value"
-		  }
-		}
-	  };
 
 		console.log(result);
 		res.send(result);
